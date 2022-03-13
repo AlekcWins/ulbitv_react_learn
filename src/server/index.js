14b0d0ce3cd83b734f44cbb56bbcb700056dac7d
Alexandr <aleksand2000@gmail.com>
2022-03-13 22:15:09 +0400

    edited readme file add instructions install for client & server

--- src/server/index.js
+++ src/server/index.js
@@ -3,7 +3,7 @@ const express = require("express")
 const cors = require('cors')
 const fileUpload = require('express-fileupload')
 const path = require('path')
-
+const bodyParser = require('body-parser');
 const sequelize = require('./db')
 require('./models/models')
 const router = require('./routes/index')
@@ -16,6 +16,13 @@ const PORT = process.env.PORT || 5000
 const app = express()
 app.use(cors())
 app.use(express.json())
+
+//For url encoded form
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({ extended: true }));
+//END For url encoded form
+
 app.use(express.static(path.resolve(__dirname, 'static')))
 app.use(fileUpload({}))
 app.use('/api', router)
